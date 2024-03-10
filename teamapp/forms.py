f44d398795cf7ae92d136df11082f33147094d9b
devvspaces <netrobepy@gmail.com>
2024-03-10 18:37:52 +0100

    update

--- teamapp/forms.py
+++ teamapp/forms.py
@@ -1,23 +1,31 @@
 from typing import Any
 from django import forms
-from .models import Profile, User
+from .models import Profile, User, Role
+
 
 class TeamMemberForm(forms.Form):
-	class Meta:
-		models = (Profile, User)
-		fields = ['user.first_name', 'user.last_name', 'user.email', 'phone_number', 'role' ]
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+    email = forms.EmailField()
+    phone_number = forms.CharField(max_length=20)
+    role = forms.IntegerField()
 
-	
-	def save(self, commit: bool = ...) -> Any:
-		user = super().save(commit=False)# Partially save the TeamMember object without committing to the database
-		username = f'{user.first_name}.{user.last_name}' # Generate a username from the first and last name
-		name_user=User.objects.create_user(username=username)# Create a new Django User object with the generated username
-		if User.admin: # If the role is "admin", grant staff and superuser permissions
-			name_user.is_staff = True
-			name_user.is_superuser = True
-			name_user.save()
+    def clean_role(self):
+        value = self.cleaned_data.get('role')
+        exists = Role.objects.filter(id=value)
+        if not exists:
+            raise forms.ValidationError('Selected role is not valid')
+        return exists.first()
 
-		user.user=name_user # Assign the created User object to the TeamMember object's "user" field
-		user.save()
-
-		return user
+    def save(self, *args, **kwargs):
+        user = User.objects.create(
+            first_name=self.cleaned_data.get('first_name'),
+            last_name=self.cleaned_data.get('last_name'),
+            email=self.cleaned_data.get('email'),
+        )
+        member = Profile.objects.create(
+            user=user,
+            role=self.cleaned_data.get('role'),
+            phone_number=self.cleaned_data.get('first_name'),
+        )
+        return member
